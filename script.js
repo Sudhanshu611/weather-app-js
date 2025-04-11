d10c93d6f49eaa61a636b06442780bba063c30e7
Sudhanshu Singh <sudhanshu0300@gmail.com>
2025-04-11 11:58:16 +0530

    Script.JS Updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = '70b3b33c2cbc16adbec6ae44263eafed';
+const API_KEY = '';
 const URL = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}&units=metric`;
 
 
